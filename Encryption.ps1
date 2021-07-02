55bed33547d7c601eb0bbbd152a6dbec8f898389
Lubos Urban <Lubos_Urban@tempest.sk>
2021-07-02 21:45:05 +0200

    Initial load..

---
+++ Encryption.ps1
@@ -0,0 +1,88 @@
+﻿# --------------------------------------------------------
+# Function: ConvertTo-UnsecureString
+# --------------------------------------------------------
+function ConvertTo-UnsecureString
+{
+    Param
+    (
+        [Parameter(Mandatory=$True)]
+        [SecureString] $secureString
+    )
+
+    $credential = New-Object System.Management.Automation.PSCredential("UserName", $secureString)
+    return($credential.GetNetworkCredential().Password)
+}
+
+# --------------------------------------------------------
+# Function: Get-EncryptionKeyByteArray
+# --------------------------------------------------------
+function Get-EncryptionKeyByteArray
+{
+    Param
+    (
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String] $encryptionKey
+    )
+
+    # Byte array (24 bytes)
+    $secureKey24Bytes = [byte[]]::new(24)
+
+    # Convert string to byte array
+    $secureKeyByteArray = [System.Text.Encoding]::Unicode.GetBytes($encryptionKey)
+
+    # Copy first 24 bytes to new array
+    $minLength = [Math]::Min($secureKey24Bytes.Length, $secureKeyByteArray.Length)
+    for ($i = 0; $i -lt $minLength; $i++)
+    {
+        $secureKey24Bytes[$i] = $secureKeyByteArray[$i]
+    }
+
+    return($secureKey24Bytes)
+}
+
+# --------------------------------------------------------
+# Function: Get-EncryptedSecureString
+# --------------------------------------------------------
+function Get-EncryptedSecureString
+{
+    Param
+    (
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [SecureString] $secureString,
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String] $encryptionKey
+    )
+
+    # Byte array
+    $secureKeyByteArray = Get-EncryptionKeyByteArray -encryptionKey $encryptionKey
+    # Encrypt secure strint to standard encoded string
+    $encryptedString = ConvertFrom-SecureString -secureString $secureString -key $secureKeyByteArray
+
+    return($encryptedString)
+}
+
+# --------------------------------------------------------
+# Function: Get-DecryptedSecureString
+# --------------------------------------------------------
+function Get-DecryptedSecureString
+{
+    Param
+    (
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String] $encryptedString,
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [String] $encryptionKey
+    )
+
+    # Byte array
+    $secureKeyByteArray = Get-EncryptionKeyByteArray -encryptionKey $encryptionKey
+    # Decrypt encoded string to secure string
+    $secureString = ConvertTo-SecureString -string $encryptedString -key $secureKeyByteArray
+
+    return($secureString)
+}
---
+++ Example.ps1
@@ -0,0 +1,22 @@
+﻿# ---------------------------------------------
+# Includes
+# ---------------------------------------------
+. "$PSScriptRoot\Encryption.ps1"
+
+# =============================================
+# Script body
+# =============================================
+# Variables declaration
+$key = "SVFNe71RfHqme9qBKKtH"
+
+# Password encryption
+$passwordSecureString = Read-Host -Prompt "Enter password" -AsSecureString
+$encryptedString = Get-EncryptedSecureString -secureString $passwordSecureString -encryptionKey $key
+Write-Host "`nEncrypted password:" -ForegroundColor Yellow
+Write-Host $encryptedString
+
+# Password decryption
+$decryptedSecureString = Get-DecryptedSecureString -encryptedString $encryptedString -encryptionKey $key
+$decryptedPassword = ConvertTo-UnsecureString -secureString $decryptedSecureString
+Write-Host "`nDecrypted password:" -ForegroundColor Yellow
+Write-Host "$decryptedPassword`n"
